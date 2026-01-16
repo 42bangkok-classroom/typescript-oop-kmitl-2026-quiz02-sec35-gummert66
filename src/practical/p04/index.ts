12d87f80bddd995aa4df8ca61101cd337b68c394
siwaphon <68011077@kmitl.ac.th>
2026-01-16 10:40:09 +0700

    p04

--- src/practical/p04/index.ts
+++ src/practical/p04/index.ts
@@ -1,1 +1,33 @@
-export function countCommentsByPost() {}
+import axios from 'axios';
+
+interface Comment {
+  postId?: number | null;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+}
+
+interface CommentCounts {
+  [key: number]: number;
+}
+
+export const countCommentsByPost = async (): Promise<CommentCounts> => {
+  try { 
+    const { data } = await axios.get<Comment[]>('https://jsonplaceholder.typicode.com/comments');
+
+    if (!data || data.length === 0) {
+      return {}; 
+    }
+
+    return data.reduce((acc, comment) => {
+      if (comment.postId != null) {
+        acc[comment.postId] = (acc[comment.postId] || 0) + 1;
+      }
+      return acc;
+    }, {} as CommentCounts);
+
+  } catch (error) {
+    throw error;
+  }
+};
